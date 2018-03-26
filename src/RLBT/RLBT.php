fbb70acaa68607ccf15ecea54764cb4f5880d5ea
Kevin N. Andrews <me@kenygamer.com>
2018-03-26 17:07:38 -0300

    Update RLBT.php

--- src/RLBT/RLBT.php
+++ src/RLBT/RLBT.php
@@ -1,1 +1,30 @@
+<?php
 
+declare(strict_types=1);
+
+namespace RLBT;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class RLBT extends PluginBase{
+    /** @var Config */
+    private $tests;
+    
+    public function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    }
+    
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+        if(!$sender instanceof Player){
+            $sender->sendMessage(TextFormat::RED . "Command must be used in-game.");
+            return true;
+        }
+        if(!isset($args[0])){
+            return false;
+        }
+        
+        $name = strtolower($sender->
+        $action = strtolower($args[0]);
