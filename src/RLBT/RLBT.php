41bad12fb49a6bf5c69296b1b915142f6af39aa3
Kevin N. Andrews <me@kenygamer.com>
2018-03-26 17:15:36 -0300

    Update RLBT.php

--- src/RLBT/RLBT.php
+++ src/RLBT/RLBT.php
@@ -6,15 +6,26 @@ namespace RLBT;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
 
-class RLBT extends PluginBase{
-    /** @var Config */
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+
+use pocketmine\plugin\PluginBase;
+
+use RLBT\CollectBuildTestsTask;
+
+class RLBT extends PluginBase implements Listener{
+    /** @var array */
     private $tests;
     
     public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getServer
     }
     
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
@@ -28,3 +39,5 @@ class RLBT extends PluginBase{
         
         $name = $sender->getName();
         $action = strtolower($args[0]);
+        switch($action){
+            case "assign":
